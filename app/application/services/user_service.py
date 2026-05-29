cca7600eb211a4edabe8be53a2fc4531c5665c5a
TuichokTheF <165381384+TupichokTheF@users.noreply.github.com>
2026-05-29 15:43:53 +0000

    Чуть-чуть абстракции накинул (тест коммит)

--- app/application/services/user_service.py
+++ app/application/services/user_service.py
@@ -1,10 +1,9 @@
-from app.infrastructure.adapters.repositories import UserRepository
-from app.domain.user import User
+from app.domain.user import User, AbstractUserRepository
 
 
 class UserService:
 
-    def __init__(self, user_repo: UserRepository):
+    def __init__(self, user_repo: AbstractUserRepository):
         self._user_repo = user_repo
 
     async def get_user_info(self, user: User) -> dict:
--- app/domain/user/__init__.py
+++ app/domain/user/__init__.py
@@ -1,2 +1,3 @@
 from app.domain.user.entities import User
 from app.domain.user.value_objects import UserEmail, UserPassword, UserUsername
+from app.domain.user.repository import AbstractUserRepository
