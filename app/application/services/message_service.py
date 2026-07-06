31d2667cc4d12976f91908ab2fede9d2b8a80dd9
TuichokTheF <165381384+TupichokTheF@users.noreply.github.com>
2026-07-06 09:57:23 +0000

    Чуть поправил

--- app/application/services/message_service.py
+++ app/application/services/message_service.py
@@ -41,13 +41,23 @@ class MessageService:
 
         return MessageDTO.from_entity(message)
 
-    async def get_latest_messages_of_chat(self, user_dto: UserDTO, chat_dto: ChatDTO):
+    async def get_latest_messages_of_chat(self, user_dto: UserDTO, chat_dto: ChatDTO) -> list[MessageDTO]:
         chat = await self._chat_repo.get_chat_by_id(chat_dto.id)
         user = await self._user_repo.get_user_by_id(user_dto.id)
 
         chat.check_member(user)
+
         latest_messages = await self._messages_cache.get_last_messages_by_chat_id(chat.id)
+        if latest_messages:
+            return latest_messages
+
+        latest_messages = await self._messages_repo.get_latest_messages_by_chat_id(chat.id)
+
+        async with self._messages_cache as pipe:
+            for message in latest_messages:
+                pipe.cache_message(message)
+
+        latest_messages = [MessageDTO.from_entity(message) for message in latest_messages]
 
         return latest_messages
-
 
--- app/domain/message/repository.py
+++ app/domain/message/repository.py
@@ -8,3 +8,7 @@ class AbstractMessageRepo(ABC):
     @abstractmethod
     async def add_message(self, message: Message):
         """Метод для добавления сообщения"""
+
+    @abstractmethod
+    async def get_latest_messages_by_chat_id(self, chat_id: int):
+        """Метод для получения последних 50 сообщений из БД, в случае, если в Redis их не окажется"""
